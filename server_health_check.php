317359d28106b7cf5fa8bf3dd915181f03ec7f3e
Ed <ede@nait.ca>
2013-03-14 14:56:52 -0600

    Monitoring Changes
    Changes to enhance monitoring. Also a fix to the moodle wdsl so that the
    url is more dynamic (there were problems with php loading the wsdl)

--- server_health_check.php
+++ server_health_check.php
@@ -1,37 +1,123 @@
 <?php
+
 require('config.php');
-if ($cancel) {
-    redirect(new moodle_url('/'));
-}
-//HTTPS is required in this page when $CFG->loginhttps enabled
-$PAGE->https_required();
+//error_reporting(E_ALL | E_STRICT);
+
+$ok = "<span style='color:green;font-weight:bold'>Ok</span>";
+$notOk = "<span style='color:red;font-weight:bold'>Not Ok</span>";
+
+$openMainSpan = "<span style=''>";
+$closeMainSpan = "</span>";
+
+
+
 
 /// Check if the user has actually submitted login data to us
 $user = authenticate_user_login('localmoodleaccount', 'thisisthepassword');
 
-echo "Local Account: ";
+echo $openMainSpan."Local Account: ".$closeMainSpan;
 
-echo ($user == null) ? "Not Ok" : "Ok";
+echo ($user == null) ? $notOk : $ok;
 
 
+echo "<br/>";
 echo "<br/>";
 
 //$user = authenticate_user_login('moodle_tloginpage', 'thisisthepassword');
 $user = authenticate_user_login('moodle_eelyn', 'test12345');
 
-echo "AD Account: ";
-echo ($user == null) ? "Not Ok" : "Ok";
+echo $openMainSpan."AD Account: ".$closeMainSpan;
+echo ($user == null) ? $notOk : $ok;
 
+echo "<br/>";
 echo "<br/>";
 
 $test = is_readable($CFG->dataroot);
 
-echo "Moodle Data Folder Readable: ";
-echo ($test == 1) ? "Ok" : "Not Ok";
+echo $openMainSpan."Moodle Data Folder Readable: ".$closeMainSpan;
+echo ($test == 1) ? $ok : $notOk;
 
+echo "<br/>";
 echo "<br/>";
 
 $test = is_writable($CFG->dataroot);
 
-echo "Moodle Data Folder Writable: ";
-echo ($test == 1) ? "Ok" : "Not Ok";
+echo $openMainSpan."Moodle Data Folder Writable: ".$closeMainSpan;
+echo ($test == 1) ? $ok : $notOk;
+
+
+//We also need a service checker that will do 2 things in 1
+//Create a webservice on AAC that returns a bool. This service calls the moodle webservice 
+//and returns true if it can and false if it can't. If this page cannot call the 
+//webservice then we know that there is a problem.
+
+echo "<br/>";
+echo "<br/>";
+
+$serviceTest = true;
+
+echo $openMainSpan."Academic Tools WebServices [not yet implented]: ".$closeMainSpan;
+
+if( $serviceTest == true)
+{
+    echo $ok;
+    
+    echo "<br/>";
+    echo "<br/>";
+
+    //check return value from previous call
+    $serviceTest = true;
+
+    echo $openMainSpan."Moodle WebServices (via Academic Tools) [not yet implented]: ".$closeMainSpan;
+
+    if( $serviceTest == true)
+    {
+        echo $ok;
+    
+        echo "<br/>";
+        echo "<br/>";
+    }
+    else
+    {
+        echo $notOk;
+    }
+}
+else
+{
+    echo $notOk;
+}
+
+$client;
+
+echo $openMainSpan."Moodle WebServices (local call): ".$closeMainSpan;
+
+try
+{
+    
+    //this is needed to help debug soap error
+    //using fiddler. Comment out when not needed.
+    //$options = array( 'proxy_host' => 'localhost',
+    //                   'proxy_port' => intval(8888));
+    //$client = new soapclient($wsdl,$options);                   
+    $wsdl = $CFG->wwwroot."/wspp/wsdl_pp.php";
+    $client = new soapclient($wsdl);
+    $result = $client->login('moodle_eelyn','thisisthepassword');
+    
+    echo $ok;
+    
+}
+catch(Exception $ex)
+{
+    $message = $ex->getMessage();
+    
+    //we are not checking for propper credentials, just making sure that the services are operational.
+    if( $message == "Invalid username and / or password.")
+    {
+        echo $ok;
+    }
+    else
+    {
+        echo $notOk . ' [message: '.$message.']';
+    }
+    
+}
