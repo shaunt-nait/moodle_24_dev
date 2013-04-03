f84ee3b3206467c9309e21d69421c7864033fbc3
Dimas <dimas.ariawan@gmail.com>
2013-04-03 09:40:07 -0600

    Add capabilities

---
+++ blocks/library/db/access.php
@@ -0,0 +1,16 @@
+defined('MOODLE_INTERNAL') || die();
+
+$capabilities = array(
+    'block/library:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
+    )
+);
--- blocks/quickmail/db/access.php
+++ blocks/quickmail/db/access.php
@@ -37,5 +37,18 @@ $capabilities = array(
         'archetypes' => array(
             'manager' => CAP_ALLOW,
         )
+    ),
+    'block/quickmail:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_BLOCK,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+
+        'clonepermissionsfrom' => 'moodle/site:manageblocks'
     )
+
 );
