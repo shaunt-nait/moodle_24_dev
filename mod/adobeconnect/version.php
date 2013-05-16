d426989a8fbd543894c8f5775b0313eb2d4ce1ca
Dimas <dimas.ariawan@gmail.com>
2013-05-16 13:01:31 -0600

    Adobe connect module

---
+++ mod/adobeconnect/version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package mod_adobeconnect
+ * @author Akinsaya Delamarre (adelamarre@remote-learner.net)
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$module->version   = 2013022702;
+$module->requires  = 2012120300;
+$module->cron      = 0;
+$module->component = 'mod_adobeconnect';
+$module->maturity  = MATURITY_STABLE;
+$module->release   = '2.4.1.1';
