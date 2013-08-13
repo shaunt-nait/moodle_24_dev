97c33642a8f70d87f96438a6bf0b2e8d771bf4d7
Dimas <dimas.ariawan@gmail.com>
2013-08-13 08:53:40 -0600

    Adobe connect fix
    show recording links only, not uploaded contents

--- mod/adobeconnect/locallib.php
+++ mod/adobeconnect/locallib.php
@@ -625,7 +625,8 @@ function aconnect_get_recordings($aconnect, $folderscoid, $sourcescoid) {
                                 $recordingvac9 = $innernodelist->item($x)->attributes->getNamedItem('duration');
                                 //In AC-8 and before, the recording length info is stored as its own element
                                 $recordingvac8 = $meetingdetail->getElementsByTagName('duration')->item(0);
-                                if (!is_null($recordingvac9) || !is_null($recordingvac8)) { 
+                                //if (!is_null($recordingvac9) || !is_null($recordingvac8)) { 
+								if ((!is_null($recordingvac9) && $recordingvac9->value !== '') || !is_null($recordingvac8)) {
 
                                     $j = (int) $domnode->nodeValue;
                                     $value = (!is_null($meetingdetail->getElementsByTagName('name'))) ?
