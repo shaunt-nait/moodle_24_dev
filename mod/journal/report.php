6d62bd7eb301fc43f8de042704a511e6b112864a
shaunt-nait <shaunt@nait.ca>
2013-10-24 12:00:59 -0600

    Journal issue
    CONTRIB-3537
    Fix Journal from counting feedback from all entries. Even ones that
    weren't edited.

--- mod/journal/report.php
+++ mod/journal/report.php
@@ -74,8 +74,7 @@ if ($data = data_submitted()) {
     foreach ($feedback as $num => $vals) {
         $entry = $entrybyentry[$num];
         // Only update entries where feedback has actually changed.
-	$notempty = $vals['r'] != NULL;
-        if (($vals['r'] <> $entry->rating && $notempty) || ($vals['c'] <> addslashes($entry->entrycomment))) {
+	if (((int)$vals['r'] <> (int)$entry->rating) || ($vals['c'] <> addslashes($entry->entrycomment))) {
             $newentry = new StdClass();
             $newentry->rating     = $vals['r'];
             $newentry->entrycomment    = $vals['c'];
