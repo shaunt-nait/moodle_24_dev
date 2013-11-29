5a85805352da1b438982f4de008cc42f4463fce6
Dimas <dimas.ariawan@gmail.com>
2013-11-29 13:37:30 -0700

    Revert "Add coursefilearea repository and block"
    This reverts commit 3101e77b48d227216dd2bfeacc7023ecca50d076.

--- repository/coursefilearea/lib20.php
+++
@@ -1,9 +0,0 @@
-<?php
-class repository_coursefilearea_abs extends repository
-{
- public function type_config_form($mform)
- {
-  repository_coursefilearea::type_config_form_real($mform);
- }
-}
-?>
--- repository/coursefilearea/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-echo "Setting up config<br />";
-require_once('../../config.php');
-echo "Your wwwroot is ".$CFG->wwwroot;
-
-?>
